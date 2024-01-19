4cc68457601202293b67951575806c38007f06ee
David-mwas <dmwas704@gmail.com>
2024-01-19 17:47:00 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
   // #90E6FF
-  return <div className="text-red-500">hello</div>;
+  return (
+    <div className="">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
